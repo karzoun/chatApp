802b4a1d63a844b0015b637b3056e3e61d57c7d2
“karzoun” <karzoun_27@hotmail.com>
2020-11-14 19:44:08 -0800

    io

--- server/index.js
+++ server/index.js
@@ -10,4 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.use(router);
+
+io.on("connection", (socket) => {
+  console.log("We have a new connection!!!!!!");
+
+  socket.on("disconnect", () => {
+    console.log("User had left");
+  });
+});
+
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
