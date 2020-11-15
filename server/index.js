97a11192c0b8a8fe977fb89d7b8fbca5bb7279c0
“karzoun” <karzoun_27@hotmail.com>
2020-11-14 20:44:46 -0800

    fixing

--- server/index.js
+++ server/index.js
@@ -18,6 +18,13 @@ io.on("connection", (socket) => {
   socket.on("join", ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, Welcome to the room ${user.room}`,
+    });
+
+    socket.join(user.room);
   });
 
   socket.on("disconnect", () => {
